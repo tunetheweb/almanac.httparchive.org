13a0224917a15b7576394dca18944b7a37fc3563
Barry <barry@tunetheweb.com>
2020-08-07 09:11:05 +0100

    Improve bash checks

--- src/tools/scripts/update_timestamps_versions.sh
+++ src/tools/scripts/update_timestamps_versions.sh
@@ -13,13 +13,13 @@
 #
 
 # echo commands to screen for debugging
-set -x
+#set -x
 
 if [ "$#" -eq 1 ]; then
 COMMIT_SHA=$1
 fi
 
-if [ "{COMMIT_SHA}" = "" ]; then
+if [ "{COMMIT_SHA}" == "" ]; then
     echo "Must supply commit sha"
     exit 1
 fi
